bfc9f4a670723e9bc5588809dfe78ddbe779e567
Junjie <junjie.zhu@sjsu.edu>
2022-12-03 14:51:18 -0800

    updated inner-vm installation

---
+++ A2/scripts/virt-install-vm.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "creating inner vm 'ubuntu-vm' in KVM using virt..."
+
+os="--os-type=linux"
+cdrom="--cdrom=/var/lib/libvirt/images/ubuntu-22.04.1-desktop-amd64.iso"
+vcpu="--vcpus=4"
+ram="--ram=4096"
+name="--name=ubuntu-vm"
+disk="--disk=~/inner-vm, size=32"
+type="--virt-type=kvm"
+network="--network=default"
+graphics="--graphics=none"
+
+virt-install $os $network $disk $cdrom $cpu $ram $type $disk $graphics --name=$name
