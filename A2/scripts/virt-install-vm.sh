c933ee5384c02ba259bf44593c905deddb0fbe76
Junjie <junjie.zhu@sjsu.edu>
2022-12-03 15:06:52 -0800

    minor issue fixed

--- A2/scripts/virt-install-vm.sh
+++ A2/scripts/virt-install-vm.sh
@@ -6,9 +6,9 @@ cdrom="--cdrom=/var/lib/libvirt/images/ubuntu-22.04.1-desktop-amd64.iso"
 vcpu="--vcpus=4"
 ram="--ram=4096"
 name="--name=ubuntu-vm"
-disk="--disk=/var/lib/libvirt/images/ubuntu-vm-disk,size=32"
+#disk="--disk=/var/lib/libvirt/images/ubuntu-vm-disk,size=32"
 type="--virt-type=kvm"
 network="--network=default"
 graphics="--graphics=none"
 
-virt-install $name $os $disk $cdrom $cpu $ram $type $disk $graphics $network 
+virt-install $name $cdrom $cpu $ram $type $disk $graphics $network 
