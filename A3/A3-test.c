c49b618095da92e1a75c59e753a1646773d48c13
Junjie <junjie.zhu@sjsu.edu>
2022-12-06 17:42:19 -0800

    fixed printf statements

--- A3/A3-test.c
+++ A3/A3-test.c
@@ -33,16 +33,16 @@ main(int argc, char **argv)
 
     /*
     For CPUID(0x4FFFFFFE) and CPUID(0x4FFFFFFF)
-    test range [-1, 70] to include 3 cases:
+    test range [0, 73] to include 3 cases:
         1. ecx in valid range -> 0~34, 36~37, 39~41, 43~69
         2. ecx in invalid range(not defined in SDM) -> 35, 38, 42   
-        3. ecx in invalid range(not enabled in KVM) -> -1, 70
+        3. ecx in invalid range(not enabled in KVM) -> 70~73
         Note: above indexs referenced from textbook Combined Volume Set of Intel® 64 and IA-32 Architectures Software Developers Manuals 
         with version 09/30/2022    
     */
 
     printf("\n-----Test Leaf Node CPUID(0x4FFFFFFE)-----\n\n");
-    for(i = -1; i < 71; i++) {
+    for(i = 0; i < 71; i++) {
         eax = 0x4FFFFFFE;
         ecx = i;
         ecx_copy = ecx;
@@ -56,7 +56,7 @@ main(int argc, char **argv)
     }
 
     printf("\n-----Test Leaf Node CPUID(0x4FFFFFFF)-----\n\n");
-    for(i = -1; i < 71; i++) {
+    for(i = 0; i < 71; i++) {
         eax = 0x4FFFFFFF;
         ecx = i;
         ecx_copy = ecx;
